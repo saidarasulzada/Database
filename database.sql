e30357dcb881c030482c7d30edd1d9180a010a84
saidarasulzada <100302100+saidarasulzada@users.noreply.github.com>
2024-01-27 21:45:28 +0400

    Add files via upload

---
+++ database.sql
@@ -0,0 +1,34 @@
+create database burn_saida;
+create table users(
+    Id int auto_increment primary key,
+    Name text(255),
+    Surname text(255),
+    Email text(255),
+    Phone int,
+    Password varchar(255),
+    Created_at datetime,
+    Updated_at datetime,
+    Deleted_at datetime
+)
+
+select*from users;
+
+create table salaries(
+    Id int auto_increment primary key ,
+    User_id int,
+    Salary float,
+    Currency_type_id int,
+    Created_at datetime,
+    Updated_at datetime,
+    Deleted_at datetime
+)
+select*from salaries;
+
+create table currencies(
+Id int auto_increment primary key,
+    Name text,
+    Created_at datetime,
+    Updated_at datetime,
+    Deleted_at datetime
+)
+select*from currencies;
